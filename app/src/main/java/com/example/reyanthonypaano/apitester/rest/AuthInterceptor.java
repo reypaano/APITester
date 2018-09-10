4922bf5fc7ac4f2dec34521ad9aa2ad17c5aa566
Eagan Martin <mece.martinece@gmail.com>
2018-09-10 13:28:40 +0800

    Updated auth interceptor;

--- app/src/main/java/com/example/reyanthonypaano/apitester/rest/AuthInterceptor.java
+++ app/src/main/java/com/example/reyanthonypaano/apitester/rest/AuthInterceptor.java
@@ -1,4 +1,26 @@
 package com.example.reyanthonypaano.apitester.rest;
 
-public class AuthInterceptor {
+import java.io.IOException;
+
+import okhttp3.Credentials;
+import okhttp3.Interceptor;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class AuthInterceptor implements Interceptor {
+    private String credentials;
+
+    public AuthInterceptor(String user, String password) {
+        this.credentials = Credentials.basic(user, password);
+    }
+
+    @Override
+    public Response intercept(Chain chain) throws IOException {
+        Request request = chain.request();
+
+        Request authenticatedRequest = request.newBuilder()
+                .header("Authorization", credentials).build();
+
+        return chain.proceed(authenticatedRequest);
+    }
 }
