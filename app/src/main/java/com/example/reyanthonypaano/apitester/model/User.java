f7434537da6284ceff8a39f67277a973bb75413e
Eagan Martin <mece.martinece@gmail.com>
2018-09-10 11:46:57 +0800

    Added user model;

---
+++ app/src/main/java/com/example/reyanthonypaano/apitester/model/User.java
@@ -0,0 +1,43 @@
+package com.example.reyanthonypaano.apitester.model;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.math.BigInteger;
+
+public class User {
+    @SerializedName("user_id")
+    private BigInteger userId;
+
+    @SerializedName("username")
+    private String username;
+
+    @SerializedName("badge_no")
+    private String badgeNo;
+
+    @SerializedName("email")
+    private String email;
+
+    @SerializedName("user_type")
+    private String userType;
+
+    @SerializedName("first_name")
+    private String firstName;
+
+    @SerializedName("middle_name")
+    private String middleName;
+
+    @SerializedName("last_name")
+    private String lastName;
+
+    @SerializedName("suffix")
+    private String suffix;
+
+    @SerializedName("full_name")
+    private String fullName;
+
+    @SerializedName("access_token")
+    private String accessToken;
+
+    @SerializedName("token_type")
+    private String tokenType;
+}
