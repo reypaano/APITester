951869ad3dc5123b62d42496d805ee3ee3640567
Eagan Martin <mece.martinece@gmail.com>
2018-09-10 11:07:38 +0800

    Transferred MainActivity to a subpackage;

--- app/src/main/java/com/example/reyanthonypaano/apitester/activity/MainActivity.java
+++ app/src/main/java/com/example/reyanthonypaano/apitester/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.reyanthonypaano.apitester;
+package com.example.reyanthonypaano.apitester.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.reyanthonypaano.apitester.R;
 
 public class MainActivity extends AppCompatActivity {
 
